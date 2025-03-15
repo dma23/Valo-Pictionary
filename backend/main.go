d0a3bd787b021e8706a85be9adcf6e4b4cf6dabe
Derek Ma <mderek@hotmail.ca>
2025-03-15 05:33:28 -0400

    Setup basic webpage

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home HTTP")
+}
+
+func SetupPages() {
+	http.HandleFunc("/", Homepage)
+}
+
 func main() {
-	fmt.Printf("Hello, World")
+	fmt.Printf("Starting Page")
+	SetupPages()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
